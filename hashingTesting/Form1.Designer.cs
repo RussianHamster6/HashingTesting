3e95534a9348188897d0f6d5afed4ad522c66526
Declan Rhodes <declanrhodesss@gmail.com>
2018-03-09 11:39:40 +0000

    Completed Password hasher using the BCrypt hashing service

---
+++ hashingTesting/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace hashingTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hashSomeShit = new System.Windows.Forms.Button();
+            this.inputTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // hashSomeShit
+            // 
+            this.hashSomeShit.Location = new System.Drawing.Point(12, 53);
+            this.hashSomeShit.Name = "hashSomeShit";
+            this.hashSomeShit.Size = new System.Drawing.Size(356, 389);
+            this.hashSomeShit.TabIndex = 0;
+            this.hashSomeShit.Text = "Let\'s Hash Some S***";
+            this.hashSomeShit.UseVisualStyleBackColor = true;
+            this.hashSomeShit.Click += new System.EventHandler(this.hashSomeShit_Click);
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Location = new System.Drawing.Point(12, 27);
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.Size = new System.Drawing.Size(356, 20);
+            this.inputTxt.TabIndex = 1;
+            this.inputTxt.Text = "What S*** are we Hashing";
+            this.inputTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 454);
+            this.Controls.Add(this.inputTxt);
+            this.Controls.Add(this.hashSomeShit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hashSomeShit;
+        private System.Windows.Forms.TextBox inputTxt;
+    }
+}
+
---
+++ hashingTesting/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace hashingTesting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+         public string hashSomeShit_Click(object sender, EventArgs e)
+        { 
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(inputTxt.ToString()); //Creates the hashed password and stores as the hased password variable
+
+            bool validPassword = BCrypt.Net.BCrypt.Verify(inputTxt.ToString(), hashedPassword); //Varifies if the password is valid
+
+            return hashedPassword; 
+        }
+    }
+}
